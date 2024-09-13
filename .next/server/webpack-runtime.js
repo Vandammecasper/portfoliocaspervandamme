bb425a9c843e115eb7b1653f2a49f611fa1f0316
Casper Van Damme <casper.van.damme@student.howest.be>
2024-09-13 17:44:42 +0200

    fixed issues

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("01f8eaaaeb520360")
+/******/ 		__webpack_require__.h = () => ("935aafb16841528f")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -197,7 +197,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3aa04e9d210cb333"; }
+/******/ 		__webpack_require__.h = function() { return "245029f3224db41c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
